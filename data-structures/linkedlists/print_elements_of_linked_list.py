876e2e76a3cc91452c8b3e6a0d77b66393f0e861
vinitkantrod <vinitkantrod@rediffmail.com>
2017-12-17 13:48:30 +0530

    :sparkles: printing linkedlist

---
+++ data-structures/linkedlists/print_elements_of_linked_list.py
@@ -0,0 +1,48 @@
+class Node():
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+    
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+def printLLInNormal(finalData, head):
+    if head == None:
+        return None
+    
+    finalData.append(head.data)
+    # print(head.data)
+    printLLInNormal(finalData, head.next)
+
+def createLinkedList(array_data, llist):
+    array_data = array_data
+    if len(array_data) <= 0:
+        pass
+        # return llist
+    
+    llist.head = Node(array_data[0])
+    new_list = llist.head
+    for i in array_data[1:]:
+        temp = Node(i)
+        new_list.next = temp
+        new_list = new_list.next
+
+if __name__ == "__main__":
+    n = input()
+    finalData = []
+    for i in range(int(n)):
+        x = input()
+        if int(x) > 0:
+            inp = input()
+            data = inp.split(" ")
+            new_data = []
+            if len(data) > 0:
+                new_data = [x for x in data if x != ""]
+            if len(new_data) > 0:
+                llist = LinkedList()
+                createLinkedList(new_data, llist)
+                printLLInNormal(finalData, llist.head)
+    
+    for i in finalData:
+        print(i)
